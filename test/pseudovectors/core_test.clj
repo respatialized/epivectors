8be95a250a5a8726a71beb837bfdfa0cdbfafa9a
Andrew Foltz-Morrison <afoltzm@gmail.com>
2018-03-04 17:16:28 -0500

    Initial commit with lein template

---
+++ test/pseudovectors/core_test.clj
@@ -0,0 +1,7 @@
+(ns pseudovectors.core-test
+  (:require [clojure.test :refer :all]
+            [pseudovectors.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
