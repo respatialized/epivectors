852cc1682d871a3768437fb2b1de90528a1aaeb8
Andrew Foltz-Morrison <afoltzm@gmail.com>
2018-03-04 20:31:55 -0500

    Updating license to GPL v3; adding thi.ng dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject pseudovectors "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "A project exploring 3 concepts: position, orientation, magnitude"
+  :url "https://github.com/respatialized/pseudovectors"
+  :license {:name "GNU General Public License v3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [thi.ng/geom "0.0.1062"]])
