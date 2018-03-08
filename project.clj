bb367b3c561feeb8f1f11b83e98894ea41474349
respatialized <respatialized@users.noreply.github.com>
2018-03-08 10:22:53 -0500

    Update project.clj
    renaming project in lein

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject pseudovectors "0.1.0-SNAPSHOT"
+(defproject epivectors "0.1.0-SNAPSHOT"
   :description "A project exploring 3 concepts: position, orientation, magnitude"
-  :url "https://github.com/respatialized/pseudovectors"
+  :url "https://github.com/respatialized/epivectors"
   :license {:name "GNU General Public License v3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
